c1c99949754dda7bcc4e67f7655280cebb9e3ebf
Andrew Elkins <accounts@andrewelkins.com>
2012-04-27 16:00:22 -0700

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 XenForoAdminExtras
 ==================
+This add-on adds an additional template called EXTRA.css and an extra.js file to the admin side of the site only.
+
+The purpose being to add customization to the admin without modifying the core admin templates.
+
+Install like any other add-on; upload the files to the correct directories and import the xml.
+
+There's one additional option which allows you to increment the cache buster on the files.
+
+Thanks goes out to Naatan and his resource Template Syntax as this Listener is based on that one.
---
+++ library/AdminExtras/Listen.php
@@ -0,0 +1,65 @@
+<?php
+
+class AdminExtras_Listen
+{
+
+    static $addExternal = false;
+
+    /**
+     * Listen for post render effects so we can append externals to the header in the PAGE_CONTAINER template
+     *
+     * @param	string						$templateName
+     * @param	string						$content
+     * @param	array						array
+     * @param	XenForo_Template_Abstract	$template
+     *
+     * @return	void
+     */
+    public static function template_post_render($templateName, &$content, array &$containerData, XenForo_Template_Abstract $template)
+    {
+        if (self::$addExternal AND $templateName == 'PAGE_CONTAINER')
+        {
+            $params 	 = $template->getParams();
+            $baseUrl 	 = $params['requestPaths']['fullBasePath'];
+
+            $options 	 = XenForo_Application::get('options');
+            $version     = $options->adminExtrasJsVersion;
+
+            $append 	 = '<link rel="stylesheet" href="'.$baseUrl.'admin.php?_css/&css=EXTRA&v='.$version.'">' . "\n";
+
+            $append 	 .= '<script src="'.$baseUrl.'js/adminExtras/extra.js?v='.$version.'"></script>' . "\n";
+
+            $content 	 = str_replace('</head>', $append . '</head>', $content);
+
+            self::$addExternal = false;
+        }
+    }
+
+    /**
+     * Listen for controller pre dispatch events so we can detect when the externals are required
+     *
+     * @param	XenForo_Controller	$controller
+     * @param	string				$action
+     *
+     * @return	void
+     */
+    public static function controller_pre_dispatch(XenForo_Controller $controller, $action)
+    {
+        if (
+            ! $controller instanceof XenForo_ControllerAdmin_AdminTemplate AND
+            ! $controller instanceof XenForo_ControllerAdmin_Template AND
+            ! $controller instanceof XenForo_ControllerAdmin_Style AND
+            ! $controller instanceof XenForo_ControllerAdmin_AdminStyleProperty AND
+            (
+                ! class_exists('TMS_ControllerAdmin_Modification', false) OR
+                ! $controller instanceof TMS_ControllerAdmin_Modification
+            )
+        )
+        {
+            return;
+        }
+
+        self::$addExternal = true;
+    }
+
+}
