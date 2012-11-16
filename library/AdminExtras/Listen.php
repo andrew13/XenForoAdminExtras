f19f6b7e364558472f07c50023b305cdc0584478
Andrew Elkins <accounts@andrewelkins.com>
2012-11-16 11:07:48 -0800

    adding full width code and updating version

--- library/AdminExtras/Listen.php
+++ library/AdminExtras/Listen.php
@@ -8,28 +8,28 @@ class AdminExtras_Listen
     /**
      * Listen for post render effects so we can append externals to the header in the PAGE_CONTAINER template
      *
-     * @param	string						$templateName
-     * @param	string						$content
-     * @param	array						array
-     * @param	XenForo_Template_Abstract	$template
+     * @param   string                      $templateName
+     * @param   string                      $content
+     * @param   array                       array
+     * @param   XenForo_Template_Abstract   $template
      *
-     * @return	void
+     * @return  void
      */
     public static function template_post_render($templateName, &$content, array &$containerData, XenForo_Template_Abstract $template)
     {
-        if (self::$addExternal AND $templateName == 'PAGE_CONTAINER')
+        if (self::$addExternal AND ( $templateName == 'PAGE_CONTAINER' OR $templateName == 'LOGIN_PAGE') )
         {
-            $params 	 = $template->getParams();
-            $baseUrl 	 = $params['requestPaths']['fullBasePath'];
+            $params      = $template->getParams();
+            $baseUrl     = $params['requestPaths']['fullBasePath'];
 
-            $options 	 = XenForo_Application::get('options');
+            $options     = XenForo_Application::get('options');
             $version     = $options->adminExtrasJsVersion;
 
-            $append 	 = '<link rel="stylesheet" href="'.$baseUrl.'admin.php?_css/&css=EXTRA&v='.$version.'">' . "\n";
+            $append      = '<link rel="stylesheet" href="'.$baseUrl.'admin.php?_css/&css=EXTRA&v='.$version.'">' . "\n";
 
-            $append 	 .= '<script src="'.$baseUrl.'js/adminExtras/extra.js?v='.$version.'"></script>' . "\n";
+            $append      .= '<script src="'.$baseUrl.'js/adminExtras/extra.js?v='.$version.'"></script>' . "\n";
 
-            $content 	 = str_replace('</head>', $append . '</head>', $content);
+            $content     = str_replace('</head>', $append . '</head>', $content);
 
             self::$addExternal = false;
         }
@@ -38,23 +38,14 @@ class AdminExtras_Listen
     /**
      * Listen for controller pre dispatch events so we can detect when the externals are required
      *
-     * @param	XenForo_Controller	$controller
-     * @param	string				$action
+     * @param   XenForo_Controller  $controller
+     * @param   string              $action
      *
-     * @return	void
+     * @return  void
      */
     public static function controller_pre_dispatch(XenForo_Controller $controller, $action)
     {
-        if (
-            ! $controller instanceof XenForo_ControllerAdmin_AdminTemplate AND
-            ! $controller instanceof XenForo_ControllerAdmin_Template AND
-            ! $controller instanceof XenForo_ControllerAdmin_Style AND
-            ! $controller instanceof XenForo_ControllerAdmin_AdminStyleProperty AND
-            (
-                ! class_exists('TMS_ControllerAdmin_Modification', false) OR
-                ! $controller instanceof TMS_ControllerAdmin_Modification
-            )
-        )
+        if ( ! $controller instanceof XenForo_ControllerAdmin_Abstract )
         {
             return;
         }
